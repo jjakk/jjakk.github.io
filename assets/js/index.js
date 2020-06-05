dd62f4f31f6eeea8c710cb26f57f99632f8741cd
Jak <jaksawesome10@gmail.com>
2020-06-04 20:58:34 -0400

    space bar support

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,9 @@ function flip(){
   document.getElementById('inner').style.transform = 'rotateY(' + (counter%2 ? 0 : 180) + 'deg)';
   counter++;
 }
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        flip();
+    }
+}
